7d85bba5cc1b56242bd21b45bff02e7c50d93fc4
BryanGreener <bryancgreener@gmail.com>
2017-07-26 18:31:23 -0400

    Create file Assignment3.sln. Create menu. Create connection to db.

---
+++ Assignment3/Assignment3/Assignment3/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace ASSIGNMENT3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectDB();
+            // Testing
+            Console.WriteLine(UI());
+            Console.ReadLine();
+        }
+        private static Int16 UI()
+        {
+            bool validateInput = false;
+            Int16 menuInput = 0;
+            do
+            {
+                Console.WriteLine(
+                    "------------------------\n" +
+                    "Please select an option:\n" +
+                    "1) Add Customer\n" +
+                    "2) Add Order\n" +
+                    "3) Remove Order\n" +
+                    "4) Ship Order\n" +
+                    "5) Print Pending\n" +
+                    "6) Restock Parts\n" +
+                    "7) Exit");
+                try { menuInput = Convert.ToInt16(Console.ReadLine()); }
+                catch (FormatException e) { Console.WriteLine("Please enter a number 1-7"); }
+                finally
+                {
+                    if (menuInput < 1 || menuInput > 7)
+                    {
+                        Console.WriteLine("Please enter a number 1-7");
+                    }
+                    else { validateInput = true; }
+                }
+            }
+            while (validateInput == false);
+            return menuInput;
+        }
+
+        public static void ConnectDB()
+        {
+            string connStr = "server=localhost;user=root;database=northwind;port=3306;password=toor;";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {
+                Console.WriteLine("Connecting to MySQL...");
+                conn.Open();
+                // SELECT statement test
+                //string sql = "SELECT model, price FROM PC WHERE price>=1000.00";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    Console.WriteLine(rdr[0] + " -- " + rdr[1]);
+                }
+                rdr.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            conn.Close();
+            Console.WriteLine("Done.");
+        }
+
+    }
+}
