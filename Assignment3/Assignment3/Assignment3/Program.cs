247dbb4a43487e3e17e8af7391563e23afb5cd29
BryanGreener <bryancgreener@gmail.com>
2017-07-27 23:18:33 -0400

    Need to fix index issues with output from query.

--- Assignment3/Assignment3/Assignment3/Program.cs
+++ Assignment3/Assignment3/Assignment3/Program.cs
@@ -13,9 +13,17 @@ namespace ASSIGNMENT3
 
         static void Main(string[] args)
         {
-            string connStr = "server=localhost;user=root;database=northwind;port=3306;password=toor;";
-            MySqlConnection conn = new MySqlConnection();
-            conn.
+            Int16 menuSelect = UI();
+            switch (menuSelect)
+            {
+                case 1: AddCustomer(); break;
+                case 2: AddOrder(); break;
+                case 3: Remove(); break;
+                case 4: Ship(); break;
+                case 5: Print(); break;
+                case 6: Restock(); break;
+            }
+            Console.ReadLine();
         }
         private static Int16 UI()
         {
@@ -48,19 +56,29 @@ namespace ASSIGNMENT3
             return menuInput;
         }
 
-        public static string QueryDB(String input)
+        public static List<string> QueryDB(String input)
         {
             string connStr = "server=localhost;user=root;database=northwind;port=3306;password=toor;";
             MySqlConnection conn = new MySqlConnection(connStr);
+            List<string> output = new List<string>(); 
             try
             {
+                conn.Open();
+                MySqlCommand lengthCmd = new MySqlCommand("SELECT COUNT(*) FROM Customers;");
+                Int32 lengthInt = Convert.ToInt32(lengthCmd.ExecuteScalar());
+                conn.Close();
                 conn.Open();
                 MySqlCommand cmd = new MySqlCommand(input, conn);
                 MySqlDataReader rdr = cmd.ExecuteReader();
                 
+
                 while (rdr.Read())
                 {
-                    Console.WriteLine(rdr[0] + " -- " + rdr[1]);
+                    for (int i = 0; i < lengthInt; i++ )
+                    {
+                        output.Add(rdr.GetString(i));
+                    }
+                    //Console.WriteLine(rdr[0] + " -- " + rdr[1]);
                 }
                 rdr.Close();
             }
@@ -68,17 +86,42 @@ namespace ASSIGNMENT3
             {
                 Console.WriteLine(ex.ToString());
             }
-
+            
             conn.Close();
             Console.WriteLine("Done.");
+            return output;
         }
 
         public static void AddCustomer()
         {
+            Int32 newID = 0;
             Console.WriteLine(
                 "ADD CUSTOMER\n" +
-                "------------\n" +
-                "Company Name:");
+                "------------\n");
+            Console.WriteLine("Customer ID:");
+            String customerID = Console.ReadLine();
+            try
+            {
+                customerID = customerID.Replace(" ", String.Empty);
+                customerID = customerID.Remove(5, (customerID.Length - 5)).ToUpper();
+            }
+            catch ( Exception e )
+            {
+                try
+                {
+                    String genID = "SELECT CustomerID FROM Customers WHERE customerID NOT LIKE '%[^0-9]%' AND CustomerID <> '';";
+                    newID = Convert.ToInt32(QueryDB(genID));
+                    newID = newID++;
+                }
+                catch( Exception e2 )
+                {
+                    Console.WriteLine(e2.ToString());
+                }
+                Console.WriteLine(e.ToString());
+
+            }
+            Console.WriteLine(customerID);
+            Console.WriteLine("Company Name:");
             String customerCompanyName = Console.ReadLine();
             Console.WriteLine("Contact Name:");
             String customerContactName = Console.ReadLine();
@@ -99,8 +142,10 @@ namespace ASSIGNMENT3
             Console.WriteLine("Fax:");
             String customerFax = Console.ReadLine();
 
-            String getID = "SELECT MAX(CustomerID) FROM Customers";
-            QueryDB
+            //String getID = "SELECT FROM Customers";
+            //String customerID = QueryDB(getID);
+            //customerID++;
+            //Console.WriteLine(customerID);
         }
         public static void AddOrder()
         {
